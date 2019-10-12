78fb4e208e24a820eb724c2d09efb5334738a910
jakers1776 <jakers1776@zoho.com>
2019-10-12 05:21:21 -0700

    Create GEN_NN.py

---
+++ GEN_NN.py
@@ -0,0 +1,53 @@
+#Genetic template network
+
+import mlrose
+import numpy as np
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+from sklearn.metrics import accuracy_score
+from sklearn import datasets
+# Load the dataset
+data = load_iris()
+
+# Split data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(data.data, data.target,
+                                                    test_size = 0.2, random_state = 3)
+
+# Normalize feature data
+scaler = MinMaxScaler()
+
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# One hot encode target values
+one_hot = OneHotEncoder()
+
+y_train_hot = one_hot.fit_transform(y_train.reshape(-1, 1)).todense()
+y_test_hot = one_hot.transform(y_test.reshape(-1, 1)).todense()
+
+#Neural Network
+
+# Initialize neural network object and fit object - attempt 1
+lr_nn_model1 = mlrose.NeuralNetwork(hidden_nodes = [100], activation = 'sigmoid', 
+                                    algorithm = 'genetic_alg', 
+                                    max_iters = 10000, bias = True, is_classifier = True, 
+                                    learning_rate = 0.001, early_stopping = True, 
+                                    clip_max = 5, max_attempts = 100, random_state = 3)
+
+lr_nn_model1.fit(X_train_scaled, y_train_hot)
+
+# Predict labels for train set and assess accuracy
+y_train_pred = lr_nn_model1.predict(X_train_scaled)
+
+y_train_accuracy = accuracy_score(y_train_hot, y_train_pred)
+
+print('Train accuracy: ',y_train_accuracy)
+
+# Predict labels for test set and assess accuracy
+y_test_pred = lr_nn_model1.predict(X_test_scaled)
+
+y_test_accuracy = accuracy_score(y_test_hot, y_test_pred)
+
+print('Test accuracy: ',y_test_accuracy)
