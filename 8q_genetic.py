3e242a91b1160788fd0f83e76d60af341be34714
jakers1776 <jakers1776@zoho.com>
2019-10-12 16:12:35 -0700

    Update 8q_genetic.py

--- 8q_genetic.py
+++ 8q_genetic.py
@@ -8,11 +8,24 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 from sklearn.metrics import accuracy_score
 
+def queens_max(state): # Initialize counter
+    fitness_cnt = 0
+          # For all pairs of queens
+    for i in range(len(state) - 1):
+        for j in range(i + 1, len(state)):
+        # Check for horizontal, diagonal-up and diagonal-down attacks
+          if (state[j] != state[i]) \
+          and (state[j] != state[i] + (j - i)) and (state[j] != state[i] - (j - i)):
+                           # If no attacks, then increment counter
+            fitness_cnt += 1 
+    return fitness_cnt
+# Initialize custom fitness function object
+fitness_cust = mlrose.CustomFitness(queens_max)
 
 # Initialize fitness function object using pre-defined class
 fitness = mlrose.Queens()
 # Define optimization problem object
-problem = mlrose.DiscreteOpt(length = 8, fitness_fn = fitness, maximize=True, max_val=8)
+problem = mlrose.DiscreteOpt(length = 8, fitness_fn = fitness_cust, maximize=True, max_val=8)
 # Define decay schedule
 schedule = mlrose.ExpDecay()
 # Start Timer
